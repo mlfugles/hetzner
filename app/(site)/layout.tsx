cdc66f6c41a34267818824cf171203d460be82b6
Marianne Fuglestad <marianne@visito.no>
2026-09-23 16:35:04 +0200

    Make another dummy change to trigger redeploy

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -35,7 +35,7 @@ export default async function SiteLayout({
           {settings?.email && (
             <a href={`mailto:${settings.email}`}>{settings.email}</a>
           )}
-          <span>{settings?.title ?? "My Portfolio!"}</span>
+          <span>{settings?.title ?? "Portfolio"}</span>
         </footer>
       </div>
       <SanityLive />
